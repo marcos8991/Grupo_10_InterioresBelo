eb9b69cfbe2ef106a4fedd52b0c8ef95316be56c
unknown <rlucila438@gmail.com>
2021-08-20 10:49:08 -0300

    agrega vista home

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ app.listen (port, ()=> console.log( 'Servidor corriendo en el puerto '+ port));
 app.use (express.static('public'));
 
 app.get('/', (req, res)=> res.sendFile(path.join(__dirname, 'views', 'index.html')));
+app.get('/home', (req, res)=> res.sendFile(path.join(__dirname, 'views', 'index.html')))
 app.get ('/login.html', (req, res)=> res.sendFile(path.join(__dirname, 'views', 'login.html')));
 app.get ('/register.html', (req, res)=> res.sendFile(path.join(__dirname, 'views', 'register.html')));
 app.get ('/productCart.html', (req, res)=> res.sendFile(path.join(__dirname, 'views', 'productCart.html')));
