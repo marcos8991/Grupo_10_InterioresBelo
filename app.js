41542dbb3ffe9c4404abdc6a3fa84c25cead6ac5
aybelo13 <aylenbelo11@gmail.com>
2021-09-13 10:29:34 -0300

    agrego cambios carro de compras

--- app.js
+++ app.js
@@ -11,8 +11,9 @@ app.get('/home', (req, res)=> res.sendFile(path.join(__dirname, 'views', 'index.
 app.get ('/', (req, res)=> res.sendFile(path.join(__dirname, 'views', 'index.html')));
 app.get ('/login.html', (req, res)=> res.sendFile(path.join(__dirname, 'views', 'login.html')));
 app.get ('/register.html', (req, res)=> res.sendFile(path.join(__dirname, 'views', 'register.html')));
-app.get ('/productCart.html', (req, res)=> res.sendFile(path.join(__dirname, 'views', 'productCart.html')));
+app.get ('/productCart', (req, res)=> res.sendFile(path.join(__dirname, 'views', 'productCart.html')));
 app.get ('/productDetail.html', (req, res)=> res.sendFile(path.join(__dirname, 'views', 'productDetail.html')));
+app.get ('/mobileFirst', (req, res) => res.sendFile(path.join(__dirname, 'views', 'mobileFirst.html')))
 
 
 app.listen (port, ()=> console.log( 'Servidor corriendo en el puerto '+ port)); 
