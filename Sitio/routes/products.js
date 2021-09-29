7b6acc17e74e0bcb2a8832d0440a7ec9cbb50c4d
MarcosZarate <agustin_8991@outlook.com>
2021-09-29 13:26:27 -0300

    agregue poder crear, editar,y actualizar productos

--- Sitio/routes/products.js
+++ Sitio/routes/products.js
@@ -1,12 +1,31 @@
 var express = require('express');
 var router = express.Router();
 
-const { cart, detail , admin , add, edit} = require('../controllers/productsController');
+const { cart, detail , admin ,store, add, edit,update,destroy} = require('../controllers/productsController');
+
+
+
 
 router.get('/productCart', cart );
-router.get('/productDetail', detail);
+
+router.get('/productDetail/:id', detail);
+
+
+
+
+
 router.get('/admin',admin);
+
+//añadir un producto
 router.get('/add',add);
-router.get('/edit', edit);
+router.post('/add',store)
+
+//editar un producto
+router.get('/edit/:id', edit);
+router.put('/edit/:id',update)
+
+//borrar un producto
+router.delete('/destroy/:id',destroy)
+
 
 module.exports = router;
