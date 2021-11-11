70f723baa7bbc62ff64b5c0218271379b84f43ef
rlucila <rlucila438@gmail.com>
2021-11-11 00:45:12 -0300

    modifica controller add y store

--- Sitio/controllers/indexController.js
+++ Sitio/controllers/indexController.js
@@ -2,8 +2,17 @@ const fs = require('fs');
 const path = require('path');
 const products = JSON.parse(fs.readFileSync(path.join(__dirname,"..","data","products.json"),"utf-8"))
 
+const db = require ('../database/models')
+
+
 module.exports = {
     index : (req,res) => {
+
+        // db.Product.findAll()
+        // .then(products => {
+        //     return res.send(products)
+        // })
+        // .catch(error => console.log(error))
         return res.render('index',{
             product : products.find(product => product.id === +req.params.id)
         })
