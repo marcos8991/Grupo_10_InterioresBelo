79886332e4e592c5aaa0f29f1cf70a111143b4d3
Marcos Urdinola <marcosurdinola07@gmail.com>
2021-11-16 14:42:08 -0300

    Db-User/controllers done

--- Sitio/routes/users.js
+++ Sitio/routes/users.js
@@ -7,6 +7,7 @@ const notEntry = require('../middlewares/notEntry')
 const upload = require('../middlewares/multerImageUser');
 
 const {register,login,logout,processRegister,processLogin,profile,update} = require('../controllers/usersController');
+const { single } = require('../middlewares/multerImageUser');
 
 
 /* GET users listing. */
@@ -14,11 +15,11 @@ router.get('/register',notEntry,register);
 router.post('/register',registerValidator,processRegister)
 
 router.get('/login',notEntry,login);
-router.post('/login',loginValidator, processLogin)
+router.post('/login', processLogin)
 
 router.get('/logout',logout)
 
 router.get('/profile',userLoginCheck,profile)
-router.post('/profile',upload.single('avatar'),update)
+router.put('/profile',upload.single('avatar'),update)
 
 module.exports = router;
